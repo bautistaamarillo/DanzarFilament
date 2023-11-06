a18c421053efce2367fe89101ccba7fd35a1f105
gabriel <lapeiregabriel@gmail.com>
2023-11-06 03:18:43 -0300

    intento crud items

---
+++ app/Models/Item.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Tutor::class,"item_students");
+    }
+    public function payments(): BelongsToMany
+    {
+        return $this->belongsToMany(Tutor::class,"item_payments");
+    }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
